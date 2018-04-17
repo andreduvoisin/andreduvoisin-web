da6254bf144006eec73117aa2c01218ef3746f00
Andre Duvoisin <anduv13@gmail.com>
2018-04-17 00:58:37 -0500

    Add localhost migration file for local development.

---
+++ mysql/localhost-migrate.sql
@@ -0,0 +1,7 @@
+-- Uncomment the lines in this file to develop locally.
+-- TODO: Something's screwed up with editing parabola_settings. All settings reset if you edit the value at all.
+
+-- UPDATE wp_options SET option_value = replace(option_value, 'andreduvoisin.com', '127.0.0.1') WHERE option_name != 'parabola_settings';
+-- UPDATE wp_posts SET guid = replace(guid, 'andreduvoisin.com','127.0.0.1');
+-- UPDATE wp_posts SET post_content = replace(post_content, 'andreduvoisin.com', '127.0.0.1');
+-- UPDATE wp_postmeta SET meta_value = replace(meta_value,'andreduvoisin.com','127.0.0.1');
