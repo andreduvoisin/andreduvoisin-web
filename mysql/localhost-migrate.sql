843c14311f4a24e0f8dd530a96eced11793040fd
Andre Duvoisin <anduv13@gmail.com>
2018-04-24 00:16:23 -0500

    Revert "Add export database script. Update main email address to test."
    This reverts commit 496d466e49a68d6818a974be46a1f2b3f81d961c.

--- mysql/localhost-migrate.sql
+++ mysql/localhost-migrate.sql
@@ -1,4 +1,3 @@
--- If anything is changed here, you must also perform the reverse in scripts/export-database.sh.
 -- TODO: Something's screwed up with editing parabola_settings. All settings reset if you edit the value at all.
 UPDATE wp_options SET option_value = replace(option_value, 'https://andreduvoisin.com', 'http://localhost:8080') WHERE option_name != 'parabola_settings';
 UPDATE wp_posts SET guid = replace(guid, 'https://andreduvoisin.com','http://localhost:8080');
