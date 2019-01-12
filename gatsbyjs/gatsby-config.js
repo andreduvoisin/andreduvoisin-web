d956789614bd69565988c3e69d58b96893ab4599
Andre Duvoisin <anduv13@gmail.com>
2019-01-12 12:15:26 -0600

    Change config and copyright to myself.

--- gatsbyjs/gatsby-config.js
+++ gatsbyjs/gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Andre Duvoisin`,
+    author: `Andre Duvoisin`,
+    description: `Personal website, portfolio, and blog.`,
+    siteUrl: `https://andreduvoisin.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `andreduvoisin`,
     },
   },
   plugins: [
