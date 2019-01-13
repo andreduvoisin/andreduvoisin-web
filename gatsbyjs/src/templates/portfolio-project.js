7efbd35b66efbdc8eb9e57975a2aea5d8a31ea96
Andre Duvoisin <anduv13@gmail.com>
2019-01-12 18:04:01 -0600

    Add portfolio project page template.

---
+++ gatsbyjs/src/templates/portfolio-project.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+
+class PortfolioProjectTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark
+    const siteTitle = this.props.data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title={post.frontmatter.title} description={post.excerpt} />
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </Layout>
+    )
+  }
+}
+
+export default PortfolioProjectTemplate
+
+export const pageQuery = graphql`
+  query PortfolioProjectBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
